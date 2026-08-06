0d44c449ea6bc06db8009f951c157c06abae94cc
shadou09090 <gnikolas155@gmai.com>
2026-08-06 11:43:42 -0500

    Initial commit backend

---
+++ src/test/java/com/project/back_end_proyecto/BackEndProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.back_end_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
